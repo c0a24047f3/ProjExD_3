f53f6e9a5b4c580ac4d952e19264785c6de30c0d
c0a24047f3 <coa24047f3@edu.teu.ac.jp>
2025-10-07 15:39:15 +0900

    演習1 score

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -140,12 +140,33 @@ class Bomb:
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
+class Score:
+    """
+    スコアを表示するクラス
+    """
+    def __init__(self):
+        """
+        スコアの文字やフォントなどを作成
+        """
+        self.ten = 0
+        self.fonto = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
+        self.img = self.fonto.render(f"スコア:{self.ten}", 0, (0,0,255))
+        self.rct = self.img.get_rect()
+        self.rct.center = (100, HEIGHT - 50)
+    def update(self, screen: pg.Surface):
+        """
+        スコアを表示する
+        引数:screen: 画面Surface
+        """
+        self.img = self.fonto.render(f"スコア:{self.ten}", 0, (0,0,255))
+        screen.blit(self.img, self.rct)
 
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
+    score = Score()
     # bomb = Bomb((255, 0, 0), 10)
     bombs = [] #爆弾のリスト
     for _ in range(NUM_OF_BOMS):
@@ -180,6 +201,7 @@ def main():
                         # ビームと爆弾の衝突判定
                         beam, bombs[i]  = None, None
                         bird.change_img(6, screen)
+                        score.ten += 1
             bombs = [bomb for bomb in bombs if bomb is not None]
                     
         key_lst = pg.key.get_pressed()
@@ -188,6 +210,7 @@ def main():
             beam.update(screen)
         for bomb in bombs:
             bomb.update(screen) 
+        score.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
